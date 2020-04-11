c37c190ad67e13defda5b4b12ccb9090652506d9
januszpolak <janusz.polak@interia.pl>
2020-04-11 20:06:18 +0200

    some updates

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ var navbar = document.querySelector("nav");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
+
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
     } else {
