554cc1b10b89c6ec8777a16fe74c96d7bb67f09a
januszpolak <janusz.polak@interia.pl>
2020-04-12 15:16:44 +0200

    mobile version\

--- main.js
+++ main.js
@@ -49,3 +49,40 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.mobile-nav');
+let lists = document.querySelectorAll('.mobile-nav__li');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    hamburger.classList.toggle('ham_active')
+
+});
+for (let i in lists) {
+    lists[i].onclick = function () {
+        menu.classList.toggle('active')
+        hamburger.classList.toggle('ham_active')
+    };
+}
+
+$('#me-mobile').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.me').offset().top
+    }, 1000)
+})
+$('#room-mobile').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.room').offset().top
+    }, 1000)
+})
+$('#services-mobile').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.services').offset().top
+    }, 1000)
+})
+$('#contact-mobile').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.contact').offset().top
+    }, 1000)
+})
